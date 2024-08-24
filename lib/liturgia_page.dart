86886e2f1827c0ee4999d0408988e108cd251400
tech-tiago <t.souzart@gmail.com>
2024-08-24 07:21:06 -0300

    V1.1.1

--- lib/liturgia_page.dart
+++ lib/liturgia_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show json;
+import 'package:intl/intl.dart';
 
 class LiturgiaPage extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class _LiturgiaPageState extends State<LiturgiaPage> {
   DateTime selectedDate = DateTime.now();
   Map<String, dynamic>? liturgiaData; // Permitir que seja nulo inicialmente
   bool isLoading = true;
+  final DateFormat dateFormat = DateFormat('dd/MM/yyyy'); // Formato brasileiro
 
   @override
   void initState() {
@@ -22,6 +24,9 @@ class _LiturgiaPageState extends State<LiturgiaPage> {
     setState(() {
       isLoading = true;
     });
+
+    final String formattedDate = dateFormat.format(date); // Formata a data
+    print("Data formatada: $formattedDate");
 
     final int dia = date.day;
     final int mes = date.month;
@@ -82,6 +87,7 @@ class _LiturgiaPageState extends State<LiturgiaPage> {
                   length: _getTabCount(),
                   child: Column(
                     children: [
+                      _buildLiturgiaHeader(), // Adiciona o cabeçalho
                       TabBar(
                         tabs: _buildTabs(),
                       ),
@@ -99,6 +105,44 @@ class _LiturgiaPageState extends State<LiturgiaPage> {
     );
   }
 
+Widget _buildLiturgiaHeader() {
+  final String corLiturgia = liturgiaData?['cor'] ?? 'Cor não disponível';
+  final String tempoSemana = liturgiaData?['liturgia'] ?? 'Liturgia não disponível';
+  final String dataLeitura = liturgiaData?['data'] ?? 'Data não disponível';
+  final String diaSemana = liturgiaData?['data'] ?? 'Data não disponível';
+
+  return Container(
+    padding: const EdgeInsets.all(16.0),
+    color: Colors.grey[200],
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          dataLeitura,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        Text(
+          'Cor Litúrgica: $corLiturgia',
+          
+        ),
+        SizedBox(height: 8),
+        Text(
+          '$tempoSemana | $diaSemana',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ],
+    ),
+  );
+}
+
+DateTime? _parseDate(String dateString) {
+  try {
+    return DateFormat('dd/MM/yyyy').parse(dateString);
+  } catch (e) {
+    return null;
+  }
+}
+
   int _getTabCount() {
     if (liturgiaData == null) return 0; // Retorna 0 se liturgiaData for nulo
     int count = 3; // 1 Leitura, Salmo, Evangelho
@@ -113,7 +157,7 @@ class _LiturgiaPageState extends State<LiturgiaPage> {
     return count;
   }
 
-List<Widget> _buildTabs() {
+  List<Widget> _buildTabs() {
     if (liturgiaData == null) return [];
     List<Widget> tabs = [
       Tab(text: '1 LEITURA'),
@@ -126,9 +170,9 @@ List<Widget> _buildTabs() {
     }
 
     return tabs;
-}
+  }
 
-List<Widget> _buildTabViews() {
+  List<Widget> _buildTabViews() {
     if (liturgiaData == null) return [];
     List<Widget> views = [
       _buildLeituraView('primeiraLeitura'),
@@ -141,133 +185,130 @@ List<Widget> _buildTabViews() {
     }
 
     return views;
-}
-
-
-Widget _buildLeituraView(String leituraKey) {
-  if (liturgiaData == null || !liturgiaData!.containsKey(leituraKey)) {
-    return Center(child: Text('No data available.'));
   }
 
-  final leitura = liturgiaData![leituraKey];
+  Widget _buildLeituraView(String leituraKey) {
+    if (liturgiaData == null || !liturgiaData!.containsKey(leituraKey)) {
+      return Center(child: Text('No data available.'));
+    }
 
-  if (leitura is Map) {
-    final referencia = leitura['referencia'] ?? '';
-    final titulo = leitura['titulo'] ?? 'Sem título';
-    final texto = leitura['texto'] ?? 'Sem texto disponível';
+    final leitura = liturgiaData![leituraKey];
 
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '$titulo ($referencia)',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            texto,
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 20),
-          Text(
-            '- Palavra do Senhor.',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            '- Graças a Deus.',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    );
-  } else {
-    return Center(child: Text('Formato de dados inválido.'));
-  }
-}
+    if (leitura is Map) {
+      final referencia = leitura['referencia'] ?? '';
+      final titulo = leitura['titulo'] ?? 'Sem título';
+      final texto = leitura['texto'] ?? 'Sem texto disponível';
 
-
-Widget _buildSalmoView() {
-  if (liturgiaData == null || !liturgiaData!.containsKey('salmo')) {
-    return Center(child: Text('No data available.'));
-  }
-
-  final salmo = liturgiaData!['salmo'];
-
-  if (salmo is Map) {
-    final referencia = salmo['referencia'] ?? '';
-    final refrao = salmo['refrao'] ?? 'Sem refrão';
-    final texto = salmo['texto'] ?? 'Sem texto disponível';
-
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Responsório ($referencia)',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            '— $refrao',
-            style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
-          ),
-          SizedBox(height: 10),
-          Text(
-            texto,
-            style: TextStyle(fontSize: 16),
-          ),
-        ],
-      ),
-    );
-  } else {
-    return Center(child: Text('Formato de dados inválido.'));
-  }
-}
-
-Widget _buildEvangelhoView() {
-  if (liturgiaData == null || !liturgiaData!.containsKey('evangelho')) {
-    return Center(child: Text('No data available.'));
+      return SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$titulo ($referencia)',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              texto,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '- Palavra do Senhor.',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '- Graças a Deus.',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Center(child: Text('Formato de dados inválido.'));
+    }
   }
 
-  final evangelho = liturgiaData!['evangelho'];
+  Widget _buildSalmoView() {
+    if (liturgiaData == null || !liturgiaData!.containsKey('salmo')) {
+      return Center(child: Text('No data available.'));
+    }
 
-  if (evangelho is Map) {
-    final referencia = evangelho['referencia'] ?? '';
-    final titulo = evangelho['titulo'] ?? 'Sem título';
-    final texto = evangelho['texto'] ?? 'Sem texto disponível';
+    final salmo = liturgiaData!['salmo'];
 
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '$titulo ($referencia)',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            texto,
-            style: TextStyle(fontSize: 16),
-          ),
-          SizedBox(height: 20),
-          Text(
-            '- Palavra da Salvação.',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            '- Glória a vós, Senhor.',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    );
-  } else {
-    return Center(child: Text('Formato de dados inválido.'));
+    if (salmo is Map) {
+      final referencia = salmo['referencia'] ?? '';
+      final refrao = salmo['refrao'] ?? 'Sem refrão';
+      final texto = salmo['texto'] ?? 'Sem texto disponível';
+
+      return SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Responsório ($referencia)',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '— $refrao',
+              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            ),
+            SizedBox(height: 10),
+            Text(
+              texto,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Center(child: Text('Formato de dados inválido.'));
+    }
   }
-}
 
+  Widget _buildEvangelhoView() {
+    if (liturgiaData == null || !liturgiaData!.containsKey('evangelho')) {
+      return Center(child: Text('No data available.'));
+    }
+
+    final evangelho = liturgiaData!['evangelho'];
+
+    if (evangelho is Map) {
+      final referencia = evangelho['referencia'] ?? '';
+      final titulo = evangelho['titulo'] ?? 'Sem título';
+      final texto = evangelho['texto'] ?? 'Sem texto disponível';
+
+      return SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$titulo ($referencia)',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              texto,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '- Palavra da Salvação.',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '- Glória a vós, Senhor.',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Center(child: Text('Formato de dados inválido.'));
+    }
+  }
 }
